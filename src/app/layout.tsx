21a939575205e320ddcd886d50fbe12626e424a9
Malik shahzad <sheziherry1@gmail.com>
2025-01-29 02:08:37 +0500

    updated

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -23,7 +23,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <ClerkProvider>
+   
     <CartProvider>
     <html lang="en">
       <body className={inter.className}>
@@ -34,6 +34,6 @@ export default function RootLayout({
         </body>
     </html>
     </CartProvider>
-    </ClerkProvider>
+   
   );
 }
